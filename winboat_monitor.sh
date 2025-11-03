5e34c7581bad852e4292cf99d6654f47e941515d
ameeno <ameeno@users.noreply.github.com>
2025-11-03 22:37:36 +0000

    fixing errors
    bla
    
    fixing logging
    
    fix many issues
    
    test repair
    
    fiix rdp monitor
    
    multiple rdp tools
    
    fixing errors
    
    fix shebang
    add wait for container
    fix running issues exit
    
    Update winboat_monitor.sh
    
    fix
    
    dupe

--- winboat_monitor.sh
+++ winboat_monitor.sh
@@ -1,8 +1,7 @@
-
 #!/bin/sh
 
 # Configuration variables
-RDP_PROCESS_NAME="xfreerdp3"
+RDP_PROCESS_NAME="^xfreerdp"
 APP_PROCESS_NAME="winboat"
 DOCKER_CONTAINER_NAME="WinBoat"
 TIMEOUT=300 # 5 minutes
@@ -15,19 +14,36 @@ RUNNING=true
 stop_container() {
     echo "Timeout reached. Stopping container $DOCKER_CONTAINER_NAME."
     docker container stop "$DOCKER_CONTAINER_NAME"
+    RUNNING=false
 }
+
+# Function to wait for the container to start
+wait_for_container() {
+    echo "Container $DOCKER_CONTAINER_NAME is not running. Waiting for it to start."
+    while true; do
+        sleep 60
+        if docker inspect --format='{{.State.Running}}' "$DOCKER_CONTAINER_NAME" 2>/dev/null | grep -q "true"; then
+            echo "Container $DOCKER_CONTAINER_NAME has started. Resuming monitoring."
+            LAST_SEEN_TIME=0
+            RUNNING=true
+            break
+        fi
+    done
+}
+
+echo "winboat_monitor is starting with PID: $$ at $(date)"
 
 # Main loop
 while $RUNNING; do
-    # Check if the container is still running. If not, exit the script.
+    # Check if the container is running.
     if ! docker inspect --format='{{.State.Running}}' "$DOCKER_CONTAINER_NAME" 2>/dev/null | grep -q "true"; then
-        echo "Container $DOCKER_CONTAINER_NAME is not running. Exiting."
-        exit 0
+        wait_for_container
+        continue # Restart the loop to re-check the container status
     fi
 
     # Check if either of the processes are running inside the container
     # The `-q` flag for pgrep makes it quiet, returning 0 on success.
-    pgrep -x "$APP_PROCESS_NAME" >/dev/null || pgrep -x "$RDP_PROCESS_NAME" >/dev/null
+    pgrep -x "$APP_PROCESS_NAME" >/dev/null || pgrep "$RDP_PROCESS_NAME" >/dev/null
     PROCESS_RUNNING=$?
 
     if [ "$PROCESS_RUNNING" -eq 0 ]; then
@@ -51,7 +67,7 @@ while $RUNNING; do
 
         if [ "$ELAPSED_TIME" -ge "$TIMEOUT" ]; then
             stop_container
-            RUNNING=false # Set flag to exit the loop
+            wait_for_container
         fi
     fi
 
